22e77786d270cd5729c98d13bf907d779ef715cc
andres <andr3sm4ca@gmail.com>
2026-05-27 22:33:58 -0500

    funcoines nivel 2primer edicion

---
+++ src/model/niveles/NivelDosCenital.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "NivelBase.h"
+
+class NivelDosCenital : public NivelBase {
+public:
+    int numero() const override;
+    void cargar(GestorNivel& gestor) override;
+    void actualizar(GestorNivel& gestor, float dt, const Control& control) override;
+    const char* nombre() const override;
+};
